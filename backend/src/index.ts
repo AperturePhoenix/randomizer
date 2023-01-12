9816c08b6ef649a17c7b838fbf5653f09cfbb73a
Lance Judan <aperture.phoenix@gmail.com>
2023-01-11 22:33:53 -0700

    setup eslint, airbnb rules, and prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express'
 
-const app: Express = express();
-const port = 3000;
+const app: Express = express()
+const port = 3000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
